12fe70cdb5babc2fb05749deccbeaef97b7e44ff
sevro49 <21120205303@ismu.edu.tr>
2023-04-07 02:31:28 +0300

    added play music function

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const wrapper = document.querySelector(".wrapper"),
     mainAudio = wrapper.querySelector("#main-audio"),
     playPauseBtn = wrapper.querySelector(".play-pause");
 
-let musicIndex = 4;
+let musicIndex = 5;
 
 window.addEventListener("load", () => {
     loadMusic(musicIndex); // calling load music function once window loaded
@@ -17,6 +17,12 @@ function loadMusic(indexNumb) {
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
     musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
     mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+//play music function
+function playMusic(){
+    wrapper.classList.add("paused");
+    mainAudio.play();
 }
 
 // play or pause music button event
