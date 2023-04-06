19661b4f76bbe9dcc32aa24c567de02d5ac4fd0c
sevro49 <21120205303@ismu.edu.tr>
2023-04-07 02:23:05 +0300

    added play or pause button click event

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-
 const wrapper = document.querySelector(".wrapper"),
-musicImg = wrapper.querySelector(".img-area img"),
-musicName = wrapper.querySelector(".song-details .name"),
-musicArtist = wrapper.querySelector(".song-details .artist"),
-mainAudio = wrapper.querySelector("#main-audio")
+    musicImg = wrapper.querySelector(".img-area img"),
+    musicName = wrapper.querySelector(".song-details .name"),
+    musicArtist = wrapper.querySelector(".song-details .artist"),
+    mainAudio = wrapper.querySelector("#main-audio"),
+    playPauseBtn = wrapper.querySelector(".play-pause");
 
 let musicIndex = 4;
 
-window.addEventListener("load", () =>{
+window.addEventListener("load", () => {
     loadMusic(musicIndex); // calling load music function once window loaded
 });
 
-
 // Load music function
-function loadMusic(indexNumb){
+function loadMusic(indexNumb) {
     musicName.innerText = allMusic[indexNumb - 1].name;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
     musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
     mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
 }
+
+// play or pause music button event
+playPauseBtn.addEventListener("click", () => {
+    const isMusicPaused = wrapper.classList.contains("paused");
+
+    // If isMusicPaused is true then call pauseMusic, else call playMusic
+    isMusicPaused ? pauseMusic() : playMusic();
+});
