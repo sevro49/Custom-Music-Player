723db5a5583568c33bd29b1cda05449bd2138425
sevro49 <21120205303@ismu.edu.tr>
2023-04-07 02:17:38 +0300

    added loadMusic function to change music by index

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio")
+
+let musicIndex = 4;
+
+window.addEventListener("load", () =>{
+    loadMusic(musicIndex); // calling load music function once window loaded
+});
+
+
+// Load music function
+function loadMusic(indexNumb){
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
